e9c4aa70adadfdbb8eb588f194b25648a161da93
ran.liao <ran.liao@mujin.co.jp>
2024-03-14 14:02:47 +0900

    Deep copy the positional arguments as well

--- python/mujinwebstackclient/webstackgraphclientutils.py
+++ python/mujinwebstackclient/webstackgraphclientutils.py
@@ -198,7 +198,7 @@ class LazyGraphQuery(webstackclientutils.LazyQuery):
         """
         # save the query function and all parameters
         self._queryFunction = queryFunction
-        self._queryArgs = args
+        self._queryArgs = copy.deepcopy(args)
         self._queryKwargs = copy.deepcopy(kwargs)
 
         # initialize limit and offset
