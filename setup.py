c8712041118d224cf9498e365e17179cedbd2b0f
Taiju Yamada <taiju.yamada@mujin.co.jp>
2026-05-01 16:57:49 +0900

    Setup pyproject.toml

--- setup.py
+++ setup.py
@@ -12,32 +12,9 @@ exec(open('python/mujinwebstackclient/version.py').read(), version)
 
 setup(
     distclass=Distribution,
-    name='mujinwebstackclient',
-    version=version['__version__'],
-    packages=['mujinwebstackclient'],
-    package_dir={'mujinwebstackclient': 'python/mujinwebstackclient'},
-    package_data={
-        'mujinwebstackclient': ['py.typed'],
-    },
-    data_files=[
-        # using scripts= will cause the first line of the script being modified for python2 or python3
-        # put the scripts in data_files will copy them as-is
-        (
-            'bin',
-            [
-                'bin/mujin_webstackclientpy_applyconfig.py',
-                'bin/mujin_webstackclientpy_runshell.py',
-                'bin/mujin_webstackclientpy_downloaddata.py',
-            ],
-        ),
-    ],
     locale_dir='locale',
-    license='Apache License, Version 2.0',
-    long_description=open('README.md').read(),
+    license='Apache License, Version 2.0',  # license must be kept in setup.py unless packaging>=24.2 is adopted
     # flake8 compliance configuration
     enable_flake8=True,  # Enable checks
     fail_on_flake=True,  # Fail builds when checks fail
-    install_requires=[
-        'websockets==15.0.1',
-    ],
 )
